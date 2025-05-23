8913ab65b91ce6536c6d785d9d4f8773300de85f
Sebastian Lucero <seba.luceroguzman@gmail.com>
2025-05-23 17:36:56 -0400

    Creacion Carrusel

---
+++ src/components/Carrusel.jsx
@@ -0,0 +1,52 @@
+import Carousel from "react-bootstrap/Carousel";
+import goku from "./Imagenes/goku.jpeg";
+import eren from "./Imagenes/eren.jpeg";
+import "./CSS/Carrusel.css";
+function Carrusel() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carrusel-img"
+          src={goku}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p className="texto">
+            Nulla vitae elit libero, a pharetra augue mollis interdum.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carrusel-img"
+          src={eren}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p className="texto">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carrusel-img"
+          src={goku}
+          alt="Third slide"
+        />{" "}
+        className= 'texto'
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p className="texto">
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Carrusel;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function ColorSchemesExample() {
   return (
@@ -9,14 +9,13 @@ function ColorSchemesExample() {
       <Navbar bg="dark" data-bs-theme="dark" expand="sm">
         <Container fluid>
           <Navbar.Brand href="#Inicio">Navbar</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='bacic-navbar-nav'>
-          <Nav className="ms-auto">
-            <Nav.Link href="#Inicio">Inicio</Nav.Link>
-            <Nav.Link href="#Servicios">Servicios</Nav.Link>
-            <Nav.Link href="#Sobre_nosotros">Sobre nosotros</Nav.Link>
-            <Nav.Link href="#Prueba">Prueba</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="bacic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#Inicio">Inicio</Nav.Link>
+              <Nav.Link href="#Servicios">Servicios</Nav.Link>
+              <Nav.Link href="#Sobre_nosotros">Sobre nosotros</Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
